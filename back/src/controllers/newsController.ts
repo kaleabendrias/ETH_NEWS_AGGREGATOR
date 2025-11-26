36bb6cd116158b7d052a2260c1d4a1709763d7d3
kaleab <kaleabendrias1212@gmail.com>
2025-11-26 15:43:27 +0300

    Add dotenv and rss-parser dependencies; refactor getNews to fetch news from RSS feeds

--- back/src/controllers/newsController.ts
+++ back/src/controllers/newsController.ts
@@ -1,30 +1,49 @@
 import { Request, Response } from "express";
 import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
+import Parser from "rss-parser";
+
+function getSourceName(url: string) {
+  if (url.includes("bbc")) return "BBC";
+  if (url.includes("aljazeera")) return "Al Jazeera";
+  if (url.includes("cnn")) return "CNN";
+  if (url.includes("reuters")) return "Reuters";
+  return "Unknown";
+}
+
 
 const getNews = async (req: Request, res: Response) => {
-  const options: AxiosRequestConfig = {
-    method: "get",
-    url: "https://google-news13.p.rapidapi.com/search",
-    params: {
-      keyword: "Ethiopia",
-      gl: "et", // Country code for Ethiopia
-      lr: "en-US", // Language preference, if needed
-    },
-    headers: {
-      "x-rapidapi-key": process.env.RAPID,
-      "x-rapidapi-host": "google-news13.p.rapidapi.com",
-    },
-  };
+  var xss_feeds: Array<string> = [
+    "https://feeds.bbci.co.uk/news/rss.xml",
+    "https://www.aljazeera.com/xml/rss/all.xml",
+    "http://rss.cnn.com/rss/edition.rss",
+    "https://feeds.reuters.com/reuters/topNews"
+  ];
 
-  axios
-    .request(options)
-    .then(function (response: AxiosResponse) {
-      console.log(response.data);
-      res.send(response.data);
-    })
-    .catch(function (error: any) {
-      console.error("Error fetching data:", error);
-    });
+  let parser = new Parser();
+  let allItems: Array<any> = [];
+
+  for (const feedUrl of xss_feeds) {
+    try {
+      const feed = await parser.parseURL(feedUrl);
+      allItems = allItems.concat(feed.items);
+    } catch (error) {
+      console.error(`Error fetching/parsing feed ${feedUrl}:`, error);
+    }
+
+    console.log(`Fetched ${allItems.length} items so far.`);;
+  }
+  allItems.sort((a, b) => {
+    return new Date(b.isoDate).getTime() - new Date(a.isoDate).getTime();
+  });
+
+  const cleaned = allItems.map(item => ({
+  title: item.title,
+  link: item.link,
+  published: item.isoDate,
+  source: getSourceName(item.link)
+}));
+
+  return res.json(cleaned)
 };
 
 module.exports = { getNews };
